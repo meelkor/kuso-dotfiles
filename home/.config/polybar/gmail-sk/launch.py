fe3b9f043c67ee3a9ab46c75f4764d53fea1a834
Jan Seda <seda@spaceknow.com>
2019-04-26 19:13:11 +0200

    oof

---
+++ home/.config/polybar/gmail-sk/launch.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import os
+import pathlib
+import subprocess
+import time
+import argparse
+from apiclient import discovery, errors
+from oauth2client import client, file
+from httplib2 import ServerNotFoundError
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--prefix', default='\uf0e0')
+parser.add_argument('-c', '--color', default='#e06c75')
+parser.add_argument('-ns', '--nosound', action='store_true')
+args = parser.parse_args()
+
+DIR = os.path.dirname(os.path.realpath(__file__))
+CREDENTIALS_PATH = os.path.join(DIR, 'credentials.json')
+
+unread_prefix = '%{F' + args.color + '}' + args.prefix + ' %{F-}'
+error_prefix = '%{F' + args.color + '}\uf06a %{F-}'
+count_was = 0
+
+def get_subject(message):
+    headers = message['payload']['headers']
+    return next(h for h in headers if h["name"] == "Subject")["value"];
+
+def get_count(count, is_odd=False):
+    tilde = '~' if is_odd else ''
+    output = ''
+    if count > 0:
+        output = unread_prefix + tilde + str(count)
+    else:
+        output = (args.prefix + ' ' + tilde).strip()
+    return output
+
+def update_count(count_was):
+    gmail = discovery.build('gmail', 'v1', credentials=file.Storage(CREDENTIALS_PATH).get())
+    messagesQuery = gmail.users().messages().list(userId='me', q="is:unread").execute()
+    count = messagesQuery["resultSizeEstimate"]
+    out = get_count(count)
+
+    if count > 0:
+        messages = messagesQuery["messages"]
+
+        message = gmail.users().messages().get(
+                userId='me',
+                id=messages[0]['id'],
+                format='metadata',
+                metadataHeaders=['Subject']).execute()
+        out += ": " + get_subject(message)
+
+    if not args.nosound and count_was < count and count > 0:
+        subprocess.run(['canberra-gtk-play', '-i', 'message'])
+
+    print(out, flush=True)
+    return count
+
+print(get_count(0), flush=True)
+
+while True:
+    try:
+        if pathlib.Path(CREDENTIALS_PATH).is_file():
+            count_was = update_count(count_was)
+            time.sleep(10)
+        else:
+            print(error_prefix + 'credentials not found', flush=True)
+            time.sleep(2)
+    except (errors.HttpError, ServerNotFoundError, OSError) as error:
+        print(get_count(count_was), flush=True)
+        time.sleep(5)
+    except client.AccessTokenRefreshError:
+        print(error_prefix + 'revoked/expired credentials', flush=True)
+        time.sleep(5)
+
