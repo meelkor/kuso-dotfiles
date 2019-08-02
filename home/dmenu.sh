27c5a499605514235d435fbe694023ae60e98abe
Jan Seda <seda@spaceknow.com>
2019-08-02 20:09:21 +0200

    Add changes from xps

--- home/dmenu.sh
+++ home/dmenu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #dmenu_run -o 0.8 -dim 0 -l 16 -nb '#336699' -nf '#eeeeee' -nb '#151515' -dc '#ffffff' -fn "RobotoMono Nerd Font Mono-14" -y 36  -sf '#000000' -sb '#4e9fb1' -w 400 -h 36 -b -i  -p '' 
-j4-dmenu-desktop --dmenu="dmenu -o 0.8 -dim 0 -l 16 -nb '#336699' -nf '#eeeeee' -nb '#151515' -dc '#ffffff' -fn 'RobotoMono Nerd Font Mono-14' -y 36  -sf '#000000' -sb '#4e9fb1' -w 400 -h 36 -b -i  -p '' "
+j4-dmenu-desktop --dmenu="dmenu -o 0.8 -dim 0 -l 16 -nb '#336699' -nf '#eeeeee' -nb '#151515' -dc '#ffffff' -fn 'NotoSans Nerd Font-14' -y 36  -sf '#000000' -sb '#4e9fb1' -w 400 -h 36 -b -i  -p '' "
 
