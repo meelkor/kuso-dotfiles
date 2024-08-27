885560d563b02c5dcac33c9db8d056c37cdb1794
Jan Seda <meelkor11@gmail.com>
2024-08-27 03:37:33 +0200

    add waybar style from stronk-dev/Tokyo-Night-Linux

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
-mkdir -p ~/.config/{kitty,sway,rofi,nvim}
+mkdir -p ~/.config/{kitty,sway,rofi,nvim,waybar}
 ln -fs ~/projects/kuso-dotfiles/config/kitty/kitty.conf ~/.config/kitty/
 ln -fs ~/projects/kuso-dotfiles/config/rofi/config.rasi ~/.config/rofi/
 ln -fs ~/projects/kuso-dotfiles/config/rofi/theme.rasi ~/.config/rofi/
 ln -fs ~/projects/kuso-dotfiles/config/sway/config ~/.config/sway/
+ln -fs ~/projects/kuso-dotfiles/config/waybar/style.css ~/.config/waybar/
 ln -fs ~/projects/kuso-dotfiles/home/.vimrc ~/
 ln -fs ~/projects/kuso-dotfiles/config/nvim/init.vim ~/.config/nvim/
 
