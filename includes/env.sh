44f8431dfef6473c95ddc3823de61c05491b285f
Jan Seda <meelkor11@gmail.com>
2025-02-11 21:49:38 +0100

    some old updates from charlotte

--- includes/env.sh
+++ includes/env.sh
@@ -6,6 +6,11 @@ export EDITOR="nvim"
 export CHROME_BIN=/usr/bin/chromium
 export TERM=kitty
 export ELECTRON_OZONE_PLATFORM_HINT=auto
+export XDG_SESSION_TYPE=wayland
+export XDG_CURRENT_DESKTOP=sway
+export QT_QPA_PLATFORM=wayland
+export QT_QPA_PLATFORMTHEME=qt5ct
+export SDL_VIDEODRIVER="wayland,x11"
 
 PATH+=":$KUSO_BACKEND/devtools"
 PATH+=":$KUSO/bin"
--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-mkdir -p ~/.config/{kitty,sway,rofi,nvim,waybar}
+mkdir -p ~/.config/{kitty,sway,rofi,nvim,waybar,dunst}
 ln -fs ~/projects/kuso-dotfiles/config/kitty/kitty.conf ~/.config/kitty/
 ln -fs ~/projects/kuso-dotfiles/config/rofi/config.rasi ~/.config/rofi/
 ln -fs ~/projects/kuso-dotfiles/config/rofi/theme.rasi ~/.config/rofi/
 ln -fs ~/projects/kuso-dotfiles/config/sway/config ~/.config/sway/
+ln -fs ~/projects/kuso-dotfiles/config/dunst/dunstrc ~/.config/dunst/
 ln -fs ~/projects/kuso-dotfiles/config/waybar/style.css ~/.config/waybar/
 ln -fs ~/projects/kuso-dotfiles/home/.vimrc ~/
 ln -fs ~/projects/kuso-dotfiles/config/nvim/init.vim ~/.config/nvim/
-
